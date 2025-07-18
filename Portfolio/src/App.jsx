fed55bedb5c7c9e8be16d8be02e982ca64269aeb
Sachinkr195 <151429353+Sachinkr195@users.noreply.github.com>
2025-07-18 15:31:28 +0530

    Update App.jsx

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -7,15 +7,10 @@ import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Review from './components/Review'
 import Allreviews from './components/Allreviews'
-import { useState, useEffect, createContext } from "react";
-import axios from 'axios'
+
 
 const App = () => {
-  useEffect(() => {
-  axios.get(`${import.meta.env.VITE_BACKEND_URL}/test`)
-    .then(res => console.log(res.data))
-    .catch(err => console.error("Connection error:", err));
-}, []);
+
   return (
     <div className='flex-col items-center justify-center'>
       <Navbar/>
